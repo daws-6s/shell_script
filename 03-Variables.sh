45538d836f2457c12a6ccd99ad4d6321c0b09c00
daws-6s <ashokmakkala16@gmail.com>
2024-09-06 15:03:56 +0530

    Date variable

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -5,4 +5,4 @@ PERSON2=Kumar
 
 echo "$PERSON1: Hi $PERSON2, GM How are you"
 echo "$PERSON2: Hello $PERSON1, GM I am good and How do you do"
-echo "$PERSON1: Yeah I am fine $PERSON2
+echo "$PERSON1: Yeah I am fine $PERSON2"
