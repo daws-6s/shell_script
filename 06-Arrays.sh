f593ea5d642a44bb325f74c05e9ab4a32927d79e
daws-6s <ashokmakkala16@gmail.com>
2024-09-06 15:53:56 +0530

    Date variable

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -2,7 +2,7 @@
 
 FRUITS=("Apple" "Banana" "Mango")
 
-echo "First fruit is: ${FRUI[0]}"
-echo "Second fruit is: ${FRUI[1]}"
-echo "Third fruit is: ${FRUI[2]}"
-echo "All Fruits: ${FRUI[@]}"
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+echo "All Fruits: ${FRUITS[@]}"
