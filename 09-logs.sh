ce7f473350abff6b77c33355f4e470dd643c8a82
daws-6s <ashokmakkala16@gmail.com>
2024-09-10 14:06:27 +0530

    first script

--- 09-logs.sh
+++ 09-logs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
 if [ $1 -ne 0 ]
@@ -20,11 +23,10 @@ else
     echo "You are a root user"
 fi
 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 
 VALIDATE $? "Installing MySql"
 
-yum install git -y
+yum install git -y &>> $LOGFILE
 
 VALIDATE $? "Installing Git"
-   
