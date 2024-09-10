a03a24071306eaa148e4090322909f812c185a23
daws-6s <ashokmakkala16@gmail.com>
2024-09-10 14:35:08 +0530

    first script

--- 09-logs.sh
+++ 09-logs.sh
@@ -5,22 +5,30 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+echo -e "$Y script execution is started $N"
+echo "total arguments:$@"
+
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-    echo "ERROR: $2 is failed"
+    echo -e "$R ERROR: $N $2 is $R failed $N"
     exit 1
 else
-    echo "$2 is Success"
+    echo -e "$2 is $G Success $N"
 fi 
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR: Please run with root user"
+    echo -e "$R ERROR: $N Please run with root user"
     exit 1
 else
-    echo "You are a root user"
+    echo -e "$G You are a root user $N"
 fi
 
 yum install mysql -y &>> $LOGFILE
